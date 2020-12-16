9723f4ff47628ade95ad299f781360b1de606f73
Guilherme Andrade Del Cantoni <guilhermeadc@gmail.com>
2020-12-16 12:24:48 -0300

    Ajuste em sequences das bases de dados

--- mysql/v3.1.0/sei_3_1_0_BD_Ref_Exec.sql
+++ mysql/v3.1.0/sei_3_1_0_BD_Ref_Exec.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.6.39, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.32, for Linux (x86_64)
 --
 -- Host: localhost    Database: sei
 -- ------------------------------------------------------
--- Server version	5.6.39
+-- Server version	5.7.32
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -6344,4 +6344,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-05-24 12:31:02
+-- Dump completed on 2020-12-16 13:02:49
